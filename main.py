4c7ba0a2a36509b9d33599281721766994349ad6
Peter Li <lihaohong6@gmail.com>
2026-06-20 23:58:40 -0400

    Adjust main.py

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from char_info.characters import save_character_info
 from char_info.counseling import save_counseling
 from char_info.ego_manifestation import save_ego_manifestations
 from char_info.favourite_gifts import save_favourite_gifts
+from char_info.partner_images import upload_partner_images
+from char_info.partners import save_partner_info
 
 
-def main():
+def char_info():
     save_character_info()
     save_ego_manifestations()
     save_favourite_gifts()
@@ -16,6 +18,16 @@ def main():
     # save_cards()
     save_counseling()
     auto_create_counseling_pages()
+
+
+def partner_info():
+    save_partner_info()
+    upload_partner_images()
+
+
+def main():
+    char_info()
+    partner_info()
 
 
 if __name__ == "__main__":
